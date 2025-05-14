ea70c370ce41510c5496131f7f7b31247187c8f1
ergitoshkezi <118963838+ergitoshkezi@users.noreply.github.com>
2025-05-14 15:42:08 +0200

    Update Es_3.m

--- Es_3.m
+++ Es_3.m
@@ -1,106 +1,126 @@
-
-
 % Load the data from the .mat file
 data = load('icecream.mat');
 
 % Extract the variables from the loaded data
-icecream2010 = data.icecream2010;
-icecream2011 = data.icecream2011;
-icecream2012 = data.icecream2012;
-icecream2013 = data.icecream2013;
-icecream2014 = data.icecream2014;
-icecream2015 = data.icecream2015;
-icecream2016 = data.icecream2016;
-icecream2017 = data.icecream2017;
-icecream2018 = data.icecream2018;
-icecream2019 = data.icecream2019;
-icecream2020 = data.icecream2020;
-icecream2021 = data.icecream2021;
-rain2010 = data.rain2010;
-rain2011 = data.rain2011;
-rain2012 = data.rain2012;
-rain2013 = data.rain2013;
-rain2014 = data.rain2014;
-rain2015 = data.rain2015;
-rain2016 = data.rain2016;
-rain2017 = data.rain2017;
-rain2018 = data.rain2018;
-rain2019 = data.rain2019;
-rain2020 = data.rain2020;
-rain2021 = data.rain2021;
-rain2022 = data.rain2022;
-temperature2010 = data.temperature2010;
-temperature2011 = data.temperature2011;
-temperature2012 = data.temperature2012;
-temperature2013 = data.temperature2013;
-temperature2014 = data.temperature2014;
-temperature2015 = data.temperature2015;
-temperature2016 = data.temperature2016;
-temperature2017 = data.temperature2017;
-temperature2018 = data.temperature2018;
-temperature2019 = data.temperature2019;
-temperature2020 = data.temperature2020;
-temperature2021 = data.temperature2021;
-temperature2022 = data.temperature2022;
+icecream2010 = data.icecream2010;  % Ice cream sales data for 2010
+icecream2011 = data.icecream2011;  % Ice cream sales data for 2011
+icecream2012 = data.icecream2012;  % Ice cream sales data for 2012
+icecream2013 = data.icecream2013;  % Ice cream sales data for 2013
+icecream2014 = data.icecream2014;  % Ice cream sales data for 2014
+icecream2015 = data.icecream2015;  % Ice cream sales data for 2015
+icecream2016 = data.icecream2016;  % Ice cream sales data for 2016
+icecream2017 = data.icecream2017;  % Ice cream sales data for 2017
+icecream2018 = data.icecream2018;  % Ice cream sales data for 2018
+icecream2019 = data.icecream2019;  % Ice cream sales data for 2019
+icecream2020 = data.icecream2020;  % Ice cream sales data for 2020
+icecream2021 = data.icecream2021;  % Ice cream sales data for 2021
 
-icecream2010 = [icecream2010, NaN];
-icecream2011 = [icecream2011, NaN];
-icecream2013 = [icecream2013, NaN];
-icecream2014 = [icecream2014, NaN];
-icecream2015 = [icecream2015, NaN];
-icecream2017 = [icecream2017, NaN];
-icecream2018 = [icecream2018, NaN];
-icecream2019 = [icecream2019, NaN];
-icecream2021 = [icecream2021, NaN];
+rain2010 = data.rain2010;  % Rain data for 2010
+rain2011 = data.rain2011;  % Rain data for 2011
+rain2012 = data.rain2012;  % Rain data for 2012
+rain2013 = data.rain2013;  % Rain data for 2013
+rain2014 = data.rain2014;  % Rain data for 2014
+rain2015 = data.rain2015;  % Rain data for 2015
+rain2016 = data.rain2016;  % Rain data for 2016
+rain2017 = data.rain2017;  % Rain data for 2017
+rain2018 = data.rain2018;  % Rain data for 2018
+rain2019 = data.rain2019;  % Rain data for 2019
+rain2020 = data.rain2020;  % Rain data for 2020
+rain2021 = data.rain2021;  % Rain data for 2021
+rain2022 = data.rain2022;  % Rain data for 2022
 
-rain2010 = [rain2010, NaN];
-rain2011 = [rain2011, NaN];
-rain2013 = [rain2013, NaN];
-rain2014 = [rain2014, NaN];
-rain2015 = [rain2015, NaN];
-rain2017 = [rain2017, NaN];
-rain2018 = [rain2018, NaN];
-rain2019 = [rain2019, NaN];
-rain2021 = [rain2021, NaN];
-rain2022 = [rain2022, NaN];
+temperature2010 = data.temperature2010;  % Temperature data for 2010
+temperature2011 = data.temperature2011;  % Temperature data for 2011
+temperature2012 = data.temperature2012;  % Temperature data for 2012
+temperature2013 = data.temperature2013;  % Temperature data for 2013
+temperature2014 = data.temperature2014;  % Temperature data for 2014
+temperature2015 = data.temperature2015;  % Temperature data for 2015
+temperature2016 = data.temperature2016;  % Temperature data for 2016
+temperature2017 = data.temperature2017;  % Temperature data for 2017
+temperature2018 = data.temperature2018;  % Temperature data for 2018
+temperature2019 = data.temperature2019;  % Temperature data for 2019
+temperature2020 = data.temperature2020;  % Temperature data for 2020
+temperature2021 = data.temperature2021;  % Temperature data for 2021
+temperature2022 = data.temperature2022;  % Temperature data for 2022
 
-temperature2010 = [temperature2010, NaN];
-temperature2011 = [temperature2011, NaN];
-temperature2013 = [temperature2013, NaN];
-temperature2014 = [temperature2014, NaN];
-temperature2015 = [temperature2015, NaN];
-temperature2017 = [temperature2017, NaN];
-temperature2018 = [temperature2018, NaN];
-temperature2019 = [temperature2019, NaN];
-temperature2021 = [temperature2021, NaN];
-temperature2022 = [temperature2022, NaN];
+% Insert NaNs for missing data points
+% Ice cream sales data
+icecream2010 = [icecream2010, NaN];   % Insert NaN for 2010
+icecream2011 = [icecream2011, NaN];   % Insert NaN for 2011
+icecream2013 = [icecream2013, NaN];   % Insert NaN for 2013
+icecream2014 = [icecream2014, NaN];   % Insert NaN for 2014
+icecream2015 = [icecream2015, NaN];   % Insert NaN for 2015
+icecream2017 = [icecream2017, NaN];   % Insert NaN for 2017
+icecream2018 = [icecream2018, NaN];   % Insert NaN for 2018
+icecream2019 = [icecream2019, NaN];   % Insert NaN for 2019
+icecream2021 = [icecream2021, NaN];   % Insert NaN for 2021
 
-% ... (load and preprocess data)
+% Rain data
+rain2010 = [rain2010, NaN];   % Insert NaN for 2010
+rain2011 = [rain2011, NaN];   % Insert NaN for 2011
+rain2013 = [rain2013, NaN];   % Insert NaN for 2013
+rain2014 = [rain2014, NaN];   % Insert NaN for 2014
+rain2015 = [rain2015, NaN];   % Insert NaN for 2015
+rain2017 = [rain2017, NaN];   % Insert NaN for 2017
+rain2018 = [rain2018, NaN];   % Insert NaN for 2018
+rain2019 = [rain2019, NaN];   % Insert NaN for 2019
+rain2021 = [rain2021, NaN];   % Insert NaN for 2021
+rain2022 = [rain2022, NaN];   % Insert NaN for 2022
+
+% Temperature data
+temperature2010 = [temperature2010, NaN];   % Insert NaN for 2010
+temperature2011 = [temperature2011, NaN];   % Insert NaN for 2011
+temperature2013 = [temperature2013, NaN];   % Insert NaN for 2013
+temperature2014 = [temperature2014, NaN];   % Insert NaN for 2014
+temperature2015 = [temperature2015, NaN];   % Insert NaN for 2015
+temperature2017 = [temperature2017, NaN];   % Insert NaN for 2017
+temperature2018 = [temperature2018, NaN];   % Insert NaN for 2018
+temperature2019 = [temperature2019, NaN];   % Insert NaN for 2019
+temperature2021 = [temperature2021, NaN];   % Insert NaN for 2021
+temperature2022 = [temperature2022, NaN];   % Insert NaN for 2022
 
 % Combine data into features and targets
 years = 2010:2021;
+
+% Combine ice cream sales data for all years
 icecream_data = [icecream2010; icecream2011; icecream2012; icecream2013; icecream2014;
                  icecream2015; icecream2016; icecream2017; icecream2018; icecream2019;
                  icecream2020; icecream2021];
+
+% Combine rain data for all years
 rain_data = [rain2010; rain2011; rain2012; rain2013; rain2014;
              rain2015; rain2016; rain2017; rain2018; rain2019;
              rain2020; rain2021];
+
+% Combine temperature data for all years
 temperature_data = [temperature2010; temperature2011; temperature2012; temperature2013; temperature2014;
                     temperature2015; temperature2016; temperature2017; temperature2018; temperature2019;
                     temperature2020; temperature2021];
 
+% Calculate the seasonal component
+% Compute the average sales for each day of the year across all years
+seasonal_component = mean(reshape(icecream_data, 366, []), 2);
+
+% Deseasonalize the ice cream sales data
+deseasonalized_icecream_data = icecream_data - repmat(seasonal_component, 1, length(years))';
+
 % Reshape data for regression models
+% Each row in X will represent a sample with columns [year, rain, temperature]
 X = [repmat(years', 366, 1), reshape(rain_data', [], 1), reshape(temperature_data', [], 1)];
-y = reshape(icecream_data', [], 1);
+
+% Reshape y to be a column vector of deseasonalized ice cream sales data across all years
+y = reshape(deseasonalized_icecream_data', [], 1);
 
 % Prepare the test set for 2022
+% X_test will have the same structure as X, but for the year 2022
 X_test = [repmat(2022, 366, 1), reshape(rain2022, [], 1), reshape(temperature2022, [], 1)];
 
 % Feature Engineering: Add interaction terms
-X = [X, X(:,2).*X(:,3)];
-X_test = [X_test, X_test(:,2).*X_test(:,3)];
+% Add an interaction term (rain * temperature) to the features
+X = [X, X(:,2) .* X(:,3)];  % Add interaction term (rain * temperature) to training data
+X_test = [X_test, X_test(:,2) .* X_test(:,3)];  % Add interaction term to test data
 
-% Define and train the models
+% Define models to train
 models = {'Linear Regression', 'SVM', 'Random Forest', 'Neural Network'};
 
 % Train models and make predictions for 2022
@@ -123,7 +143,14 @@ for i = 1:length(models)
     end
     
     % Make predictions for 2022
-    y_pred = predict(mdl, X_test);
+    if strcmp(model, 'Neural Network')
+        y_pred = net(X_test')';
+    else
+        y_pred = predict(mdl, X_test);
+    end
+    
+    % Add the seasonal component back to the predictions
+    y_pred = y_pred + seasonal_component;
     
     % Print the predictions
     fprintf('%s: Predictions for 2022 = \n', model);
